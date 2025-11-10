d868bbc02acb9a324c5069d6a7bd0460ec82a089
Hyago Oliveira <hyago.oliveira@1mbitshorde.com>
2025-11-10 14:38:32 -0300

    Add Actor SO

---
+++ Runtime/Data/Actor.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.Localization;
+
+namespace OneM.DialogueSystem
+{
+    /// <summary>
+    /// Class to hold Dialogue Actor data.
+    /// </summary>
+    [CreateAssetMenu(fileName = "Actor", menuName = "OneM/Dialogue System/New Actor", order = 110)]
+    public sealed class Actor : ScriptableObject
+    {
+        [field: SerializeField, Tooltip("The portrait image used to show the actor during dialogues.")]
+        public Sprite Portrait { get; set; }
+        [field: SerializeField, Tooltip("The loacalized name used to show the actor name during dialogues.")]
+        public LocalizedString LocalizedName { get; set; }
+    }
+}
